5deec0917283f9d7f142a61e135e1f0ef9075574
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2026-04-27 14:04:58 +0200

    feat: add connection string and DI

--- ROSCA.Infrastructure/DependencyInjection.cs
+++ ROSCA.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,17 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using ROSCA.Application.Interfaces.FundMembers;
+using ROSCA.Application.Interfaces.Funds;
+using ROSCA.Application.Interfaces.Payouts;
+using ROSCA.Application.Interfaces.Wallets;
+using ROSCA.Application.Interfaces.WalletTransactions;
 using ROSCA.Infrastructure.Persistence;
+using ROSCA.Infrastructure.Repositories.FundMembers;
+using ROSCA.Infrastructure.Repositories.Funds;
+using ROSCA.Infrastructure.Repositories.Payouts;
+using ROSCA.Infrastructure.Repositories.Wallets;
+using ROSCA.Infrastructure.Repositories.WalletTransactions;
 
 namespace ROSCA.Infrastructure
 {
@@ -10,6 +20,12 @@ namespace ROSCA.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connection)
         {
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+
+            services.AddScoped<IFundRepository, FundRepository>();
+            services.AddScoped<IFundMemberRepository, FundMemberRepository>();
+            services.AddScoped<IPayoutRepository, PayoutRepository>();
+            services.AddScoped<IWalletRepository, WalletRepository>();
+            services.AddScoped<IWalletTransactionRepository, WalletTransactionRepository>();
 
             return services;
         }

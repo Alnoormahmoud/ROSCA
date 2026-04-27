74c070486471f80da53fbb0e9c8e54c2dd2c1120
Alnoor Mahmoud <alnoormahmoud92@gmail.com>
2026-04-28 01:28:42 +1300

    feat: Add IUserServise and implement UserService with business logic

---
+++ ROSCA.Application/DTOs/Users/UserSummaryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROSCA.Application.DTOs.Users
+{
+    public  class UserSummaryDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Level { get; set; } = string.Empty;  // from Profile
+    }
+}

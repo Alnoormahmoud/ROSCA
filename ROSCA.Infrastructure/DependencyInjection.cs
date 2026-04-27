40bfa3b7de2f21db98400d53d60dbf87e7108678
Alnoor Mahmoud <alnoormahmoud92@gmail.com>
2026-04-27 22:39:34 +1300

    chore(di): register IUserRepository in dependency injection container

--- ROSCA.Infrastructure/DependencyInjection.cs
+++ ROSCA.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using ROSCA.Application.Interfaces.Users;
 using ROSCA.Infrastructure.Persistence;
+using System;
 
 namespace ROSCA.Infrastructure
 {
@@ -10,6 +11,7 @@ namespace ROSCA.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connection)
         {
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+            services.AddScoped<IUserRepository, UserRepository>();
 
             return services;
         }
